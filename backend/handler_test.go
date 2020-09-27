1fe400863560b62519e3f98f8897aea19ae45573
Shardool Pathak <shardoolpathak@gmail.com>
2020-09-26 21:02:56 -0400

    example test

--- backend/handler_test.go
+++ backend/handler_test.go
@@ -2,7 +2,66 @@ package main
 
 import (
     "net/http"
+    "bytes"
     "net/http/httptest"
     "testing"
 )
 
+
+func TestSignIn(t *testing.T) {
+    jsonData := []byte(`
+		"username":"Shardool",
+		"password":"Pathak",
+		"description":"My Bio",
+		"goalweight": 165,
+		"bodyweight": 188,
+		"caloriegoal":3500,
+		"caloriesleft":1600
+    }`)
+
+    req, err := http.NewRequest("POST", "/signin", bytes.NewBuffer(jsonData))
+
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(Signin)
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+	print(rr.Body.String())
+}
+
+func TestSignIn(t *testing.T) {
+    jsonData := []byte(`
+		"username":"Shardool",
+		"password":"Pathak",
+		"description":"My Bio",
+		"goalweight": 165,
+		"bodyweight": 188,
+		"caloriegoal":3500,
+		"caloriesleft":1600
+    }`)
+
+    req, err := http.NewRequest("POST", "/signin", bytes.NewBuffer(jsonData))
+
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(Signin)
+    handler.ServeHTTP(rr, req)
+
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+
+	print(rr.Body.String())
+}
--- backend/profile.go
+++ backend/profile.go
@@ -4,9 +4,9 @@ package main
 	User Field
 */
 type Profile struct {
+	Username string `json:"username", db:"username"`
 	Password string `json:"password", db:"password"`
 	Description string `json:"description",db:"description"`
-	Username string `json:"username", db:"username"`
 	GoalWeight float32 `json:"goalweight", db:"goalweight"` 
 	Bodyweight float32 `json:"bodyweight", db:"bodyweight"` 
 	CalorieGoal float32 `json:"caloriegoal",db"caloriegoal`
