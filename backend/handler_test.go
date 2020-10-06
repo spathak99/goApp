d0416448b29f04726d741f23c56d9439ce48e258
Shardool Pathak <shardoolpathak@gmail.com>
2020-10-06 17:30:45 -0400

    used actual http testing

--- backend/handler_test.go
+++ backend/handler_test.go
@@ -4,6 +4,7 @@ import (
     "net/http"
     "testing"
     "bytes"
+    "net/http/httptest"
     "github.com/stretchr/testify/assert"
 )
 
@@ -13,80 +14,54 @@ Testing Signin
 
 
 func TestSignin(t *testing.T){
+    //Start Server
+    signin_url := "http://localhost:8000/signin"
+    go startServer()
+    
+
+    //Test 1
     data := []byte(`{
         "username":"fake_account"
         "password":"password"
     }`)
-    signin_url := "http://localhost:8000/signin"
 
-    //Start Server
-    go startServer()
-
-
-    //Test1
-    req, err := http.NewRequest("POST", signin_url, bytes.NewBuffer(data))
+    req, err := http.NewRequest("POST",signin_url, bytes.NewBuffer(data))
     req.Header.Set("X-Custom-Header", "myvalue")
     req.Header.Set("Content-Type", "application/json")
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
     if err != nil {
         panic(err)
     }
-    //defer resp.Body.Close()
-    assert.Equal(t, "400 Bad Request", resp.Status) 
 
-    
+    w := httptest.NewRecorder()
+    handler := http.HandlerFunc(Signin)
+    handler.ServeHTTP(w, req)
+    resp := w.Result()
+
+    assert.Equal(t, 400, resp.StatusCode)
+   
     //Test 2
     data = []byte(`{
         "username":"testingaccount",
         "password":"password"
     }`)
 
-    req, err = http.NewRequest("POST", signin_url, bytes.NewBuffer(data))
+    req, err = http.NewRequest("POST",signin_url, bytes.NewBuffer(data))
     req.Header.Set("X-Custom-Header", "myvalue")
     req.Header.Set("Content-Type", "application/json")
 
-    client = &http.Client{}
-    resp, err = client.Do(req)
     if err != nil {
         panic(err)
     }
-    //defer resp.Body.Close()
-    assert.Equal(t, "200 OK", resp.Status)
+
+    w = httptest.NewRecorder()
+    handler = http.HandlerFunc(Signin)
+    handler.ServeHTTP(w, req)
+    resp = w.Result()
+
+    assert.Equal(t, 200, resp.StatusCode)
 }
 
-
-/*
-Testing Calories Update
-*/
-func TestCalorieUpdate(t *testing.T){
-   
-    //Calories URL
-    calories_url := "http://localhost:8000/update_calories"
-
-    //Test1
-    data := []byte(`{
-        "username":"testingaccount",
-        "password":"password",
-        "description":"enjoy workoiut",
-        "goalweight": 200,
-        "bodyweight": 188,
-        "caloriegoal": 4000,
-        "caloriesleft": 5000
-    }`)
-    req, err := http.NewRequest("POST", calories_url, bytes.NewBuffer(data))
-    req.Header.Set("X-Custom-Header", "myvalue")
-    req.Header.Set("Content-Type", "application/json")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
-    assert.Equal(t, "200 OK", resp.Status)
-    
-}
 
 
 
