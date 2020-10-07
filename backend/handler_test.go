789495509c927351917f54e6d0c9fb19cfff9cea
Shardool Pathak <shardoolpathak@gmail.com>
2020-10-06 22:21:08 -0400

    Initial description test

--- backend/handler_test.go
+++ backend/handler_test.go
@@ -184,3 +184,94 @@ func TestCalorieUpdate(t *testing.T){
     }
     assert.Equal(t, 200, resp.StatusCode)
 }
+
+
+
+/*
+Test Bio Update
+*/
+
+func TestDescriptionUpdate(t *testing.T){
+        //Signin
+        signin_url := "http://localhost:8000/signin"
+        data := []byte(`{
+            "username":"testingaccount",
+            "password":"password"
+        }`)
+    
+        req, err := http.NewRequest("POST",signin_url, bytes.NewBuffer(data))
+        req.Header.Set("X-Custom-Header", "myvalue")
+        req.Header.Set("Content-Type", "application/json")
+    
+        if err != nil {
+            panic(err)
+        }
+    
+        w := httptest.NewRecorder()
+        handler := http.HandlerFunc(Signin)
+        handler.ServeHTTP(w, req)
+        resp := w.Result()
+    
+    
+        bio_url := "http://localhost:8000/update_bio"
+    
+        //Test 1
+        data = []byte(`{
+            "username":"testingaccount",
+            "password":"password",
+            "description":"Test Bio 1",
+            "goalweight": 200,
+            "bodyweight": 188,
+            "caloriegoal": 4000,
+            "caloriesleft": 10
+        }`)
+    
+        req, err = http.NewRequest("POST",bio_url, bytes.NewBuffer(data))
+        req.Header.Set("X-Custom-Header", "myvalue")
+        req.Header.Set("Content-Type", "application/json")
+    
+        if err != nil {
+            panic(err)
+        }
+    
+        handler2 := http.HandlerFunc(UpdateDescription)
+        handler2.ServeHTTP(w, req)
+        resp = w.Result()
+    
+        _, err = ioutil.ReadAll(resp.Body)
+        if err != nil {
+            panic(err)
+        }
+       
+        assert.Equal(t, 200, resp.StatusCode)
+    
+        //Test 2
+        data2 := []byte(`{
+            "username":"testingaccount",
+            "password":"password",
+            "description":"Test Bio 2",
+            "goalweight": 200,
+            "bodyweight": 188,
+            "caloriegoal": 4000,
+            "caloriesleft": 10
+        }`)
+    
+        req, err = http.NewRequest("POST",bio_url, bytes.NewBuffer(data2))
+        req.Header.Set("X-Custom-Header", "myvalue")
+        req.Header.Set("Content-Type", "application/json")
+    
+        if err != nil {
+            panic(err)
+        }
+    
+        handler2.ServeHTTP(w, req)
+        resp = w.Result()
+    
+        _, err = ioutil.ReadAll(resp.Body)
+    
+        if err != nil {
+            panic(err)
+        }
+    
+        assert.Equal(t, 200, resp.StatusCode)
+}
