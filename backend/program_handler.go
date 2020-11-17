c7e6fa3fdd91e02a8bcd7ae249440518fae1fe9d
Shardool Pathak <shardoolpathak@gmail.com>
2020-11-16 23:22:00 -0500

    starting to work with program

---
+++ backend/program_handler.go
@@ -0,0 +1,29 @@
+package main
+/*
+	Deal with Programs
+*/
+
+
+
+/*
+	Grab File
+*/
+func GrabProgramFile(){
+	//TODO: Pull program file from S3 for a specified user
+}
+
+
+/*
+	Parse the File 
+*/
+func ParseProgram(){
+	//TODO: Parse the training progaram the correct way
+}
+
+
+/*
+	Sync Program with Calandar
+*/
+func CalendarSync(){
+	//TODO : Sync the parsed program with the google calandar in the correct way
+}
