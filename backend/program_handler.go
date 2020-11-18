0ef5d3236a7995ae48ee7e99888b6c1fecf9347d
Shardool Pathak <shardoolpathak@gmail.com>
2020-11-18 01:50:22 -0500

    setup aws

--- backend/program_handler.go
+++ backend/program_handler.go
@@ -1,7 +1,11 @@
 package main
-/*
-	Deal with Programs
-*/
+import (
+	"github.com/aws/aws-sdk-go"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"fmt"
+	"os"
+)
 
 
 
