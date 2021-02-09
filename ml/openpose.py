47d39b009123af55a9ab6439a1033ce25f04d9a2
Shardool Pathak <shardoolpathak@gmail.com>
2021-02-08 16:29:32 -0800

    starting pose estimation

--- ml/openpose.py
+++
@@ -1,9 +0,0 @@
-import requests
-r = requests.post(
-    "https://api.deepai.org/api/pose-detection",
-    files={
-        'image': open('squat.jpg', 'rb'),
-    },
-    headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
-)
-print(r.json())
