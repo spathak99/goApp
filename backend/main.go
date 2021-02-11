336879b89a8cdd73f401d123ea0ffa121fb51bc2
Shardool Pathak <shardoolpathak@gmail.com>
2021-02-11 11:08:08 -0800

    cors enabled

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/handlers"
+	"github.com/rs/cors"
+
 	"github.com/gorilla/sessions"
 
 	_ "github.com/lib/pq"
@@ -31,45 +30,47 @@ func main() {
 func startServer() {
 	print("Starting Server")
 
-	http.HandleFunc("/signin", Signin)
-	http.HandleFunc("/signup", Signup)
-	http.HandleFunc("/logout", Logout)
-	http.HandleFunc("/update_bio", UpdateDescription)
-	http.HandleFunc("/update_weight", UpdateWeights)
-	http.HandleFunc("/update_calories", UpdateCalories)
-	http.HandleFunc("/get_user_data", GetUserData)
-	http.HandleFunc("/get_followers", GetFollowers)
-	http.HandleFunc("/get_following", GetFollowing)
-	http.HandleFunc("/follow", Follow)
-	http.HandleFunc("/unfollow", Unfollow)
-	http.HandleFunc("/make_post", MakePost)
-	http.HandleFunc("/get_feed", GetFeed)
-	http.HandleFunc("/like_post", LikePost)
-	http.HandleFunc("/unlike_post", Unlike)
-	http.HandleFunc("/initial_custom_program", InitializeProgram)
-	http.HandleFunc("/update_custom_program", UpdateCustomProgram)
-	http.HandleFunc("/get_custom_program", GetCustomProgram)
-	http.HandleFunc("/get_personal_feed", GetPersonalFeed)
-	http.HandleFunc("/search", FuzzySearch)
-	http.HandleFunc("/update_name", UpdateName)
-	http.HandleFunc("/initialize_lifts", InitializeLifts)
-	http.HandleFunc("/update_lifts", UpdateLifts)
-	http.HandleFunc("/estimate_max", EstimateMax)
-	http.HandleFunc("/logexercise", LogExercise)
-	http.HandleFunc("/grablog", GrabLog)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/signin", Signin)
+	mux.HandleFunc("/signup", Signup)
+	mux.HandleFunc("/logout", Logout)
+	mux.HandleFunc("/update_bio", UpdateDescription)
+	mux.HandleFunc("/update_weight", UpdateWeights)
+	mux.HandleFunc("/update_calories", UpdateCalories)
+	mux.HandleFunc("/get_user_data", GetUserData)
+	mux.HandleFunc("/get_followers", GetFollowers)
+	mux.HandleFunc("/get_following", GetFollowing)
+	mux.HandleFunc("/follow", Follow)
+	mux.HandleFunc("/unfollow", Unfollow)
+	mux.HandleFunc("/make_post", MakePost)
+	mux.HandleFunc("/get_feed", GetFeed)
+	mux.HandleFunc("/like_post", LikePost)
+	mux.HandleFunc("/unlike_post", Unlike)
+	mux.HandleFunc("/initial_custom_program", InitializeProgram)
+	mux.HandleFunc("/update_custom_program", UpdateCustomProgram)
+	mux.HandleFunc("/get_custom_program", GetCustomProgram)
+	mux.HandleFunc("/get_personal_feed", GetPersonalFeed)
+	mux.HandleFunc("/search", FuzzySearch)
+	mux.HandleFunc("/update_name", UpdateName)
+	mux.HandleFunc("/initialize_lifts", InitializeLifts)
+	mux.HandleFunc("/update_lifts", UpdateLifts)
+	mux.HandleFunc("/estimate_max", EstimateMax)
+	mux.HandleFunc("/logexercise", LogExercise)
+	mux.HandleFunc("/grablog", GrabLog)
 	initDB()
-	router := http.DefaultServeMux
-
-	log.Fatal(http.ListenAndServe(":8000",
-		handlers.LoggingHandler(os.Stdout, handlers.CORS(
-			handlers.AllowedMethods([]string{"POST"}),
-			handlers.AllowedOrigins([]string{"*"}),
-			handlers.AllowedHeaders([]string{"X-Requested-With"}))(router))))
+	handler := cors.Default().Handler(mux)
+	http.ListenAndServe(":8000", handler)
 
 }
 
 func stopServer() {
 	server.Close()
+}
+
+func setupCORS(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
 func initDB() {
